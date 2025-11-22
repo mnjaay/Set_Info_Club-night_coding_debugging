024102ec3146979291871de506f95121fa4be2c0
Modou Niane <modou.niane@univ-thies.sn>
2025-11-22 23:45:17 +0000

    correction

--- facile/facile_1.c
+++ facile/facile_1.c
@@ -3,7 +3,7 @@
 int main() {
     int tab[5] = {3, 6, 9, 12, 15};
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) { // ajout de 5 au lieu de 4
         printf("%d ", tab[i]);
     }
 
--- facile/facile_2.c
+++ facile/facile_2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 float calculerPrix(float prixHT, float tva) {
-     return prixHT * (1 + tva / 100); 
+     return prixHT * (1 + tva / 100); //ajout de return
 }
 
 int main() {
     float prix = calculerPrix(100, 18);
-    printf("Prix TTC : %f\n", prix);
+    printf("Prix TTC : %f\n", prix);//ajout de %f
     return 0;
 }
 
