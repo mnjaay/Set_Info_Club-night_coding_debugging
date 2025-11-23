caba97e661e4b0b8a8bd92a9c8f2f3d0a8fb6f29
@yacine.fall2 <yacine.fall2@univ-thies.sn>
2025-11-23 00:01:41 +0000

    adding files

--- facile/facile_1.c
+++ facile/facile_1.c
@@ -2,7 +2,7 @@
 
 int main() {
     int tab[5] = {3, 6, 9, 12, 15};
-
+// error trouve etait que la condition de sortie etait 4 au lieu de 5
     for (int i = 0; i < 5; i++) {
         printf("%d ", tab[i]);
     }
--- facile/facile_2.c
+++ facile/facile_2.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 float calculerPrix(float prixHT, float tva) {
+    //error trouve return
      return prixHT * (1 + tva / 100); 
 }
 
-int main() {
+int main() { 
+  //les valeurs entrees sont de type entier alors que la fonction attend des floats 
     float prix = calculerPrix(100.0, 18.0);
+    // error troucve etait le format d'affichage  %f qui etait omis 
     printf("Prix TTC : %f", prix);  
     return 0;
 }
