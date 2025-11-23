cab44aa4a6f4fc9511d958799bbd56244911026f
Djibril-7221 <diassedjibril5@gmail.com>
2025-11-23 00:34:09 +0000

    equipeC

--- facile/facile_3.c
+++ facile/facile_3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int sommePairs(int tab[], int taille) {
-    int somme = 1;
+    int somme = 0;
     for (int i = 0; i < taille; i++) {
-        if (tab[i] / 2 == 0)
-            somme = tab[i];  
+        if (tab[i] % 2 == 0)
+            somme += tab[i];  
     }
     return somme;
 }
@@ -13,7 +13,7 @@ int main() {
     int tab[5] = {2, 3, 4, 5, 6};
     int total = sommePairs(tab, 5);
 
-    printf("Somme des nombres pairs : %d\n", total);
+    printf("Somme des nombres pairs : %d", total);
 
     return 0;
 }
