258b3cbe3e33ce22e914e1ef7f9d9de84818e169
Modou Niane <modou.niane@univ-thies.sn>
2025-11-23 00:29:16 +0000

    exo3

---
+++ facile/facile_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sommePairs(int tab[], int taille) {
+    int somme = 0;//initialiser somme à 0
+    for (int i = 0; i < taille; i++) {
+        if (tab[i] % 2 == 0)//modulo au lieu de /
+            somme += tab[i]; //modification somme += tab[i];
+    }
+    return somme;
+}
+
+int main() {
+    int tab[5] = {2, 3, 4, 5, 6};
+    int total = sommePairs(tab, 5);
+
+    printf("Somme des nombres pairs : %d\n", total);
+
+    return 0;
+}
