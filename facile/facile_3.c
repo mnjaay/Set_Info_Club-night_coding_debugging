aa10007f3d8d44dfa82f21ec0c88d4ba1d59a546
Mouhamed Ndiaye <papehamou@gmail.com>
2025-11-23 00:17:21 +0000

    mayma touti

---
+++ facile/facile_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sommePairs(int tab[], int taille) {
+    int somme = 1;
+    for (int i = 0; i < taille; i++) {
+        if (tab[i] / 2 == 0)
+            somme = tab[i];  
+    }
+    return somme;
+}
+
+int main() {
+    int tab[5] = {2, 3, 4, 5, 6};
+    int total = sommePairs(tab, 5);
+
+    printf("Somme des nombres pairs : %d\n", total);
+
+    return 0;
+}
